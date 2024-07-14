fe1b831bb30111c3c3e4dc0e2642746028b29e10
abasAlmaayofi <abasalmaayofi@gmail.com>
2024-07-14 19:08:35 +0300

    Initial Commit

---
+++ MCTS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MCTS
+//
+//  Created by Abas Almaayofi on 14/07/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
